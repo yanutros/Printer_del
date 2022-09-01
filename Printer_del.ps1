04121ef65c85c9f02b007dfe04b3fcc54e696bea
Yanutros <75123766+yanutros@users.noreply.github.com>
2022-09-01 09:11:08 +0200

    Create Printer_del.ps1

---
+++ Printer_del.ps1
@@ -0,0 +1,89 @@
+function CheckPrinter {
+
+    param (
+        $nomImp
+
+    )
+    $print = Get-Printer
+    $print2 = ($print | select name)
+    foreach($printer in $print2)
+    {
+        $printername = $printer.name
+        if ($printername -eq $nomImp)
+            {return $true}        
+    }
+    return $false
+}
+
+$PrinterXPS = CheckPrinter "Microsoft XPS Document Writer"
+$PrinterONE = CheckPrinter "OneNote for Windows 10"
+$PrinterFAX = CheckPrinter "Fax"
+$PrinterPDF = CheckPrinter "Microsoft Print to PDF"
+
+# Lancer le script en mode administrateur
+function Test-Administrator  
+{  
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+}
+
+if (-not (Test-Administrator))
+{
+    if ( Test-Path "$psHome\pwsh.exe")
+        {
+        Write-Host "pwsh présent"
+        Start-Process "$psHome\pwsh.exe" -Verb Runas -ArgumentList '-command ".\printer_del.ps1"'
+        }
+    else
+    {
+        Start-Process "$psHome\powershell.exe" -Verb Runas -ArgumentList '-command ".\printer_del.ps1"'
+    }
+}
+
+#  Regarder si l'imprimante Microsoft XPS Document Writer est presente sur le poste, sinon la desinstalle
+
+if (-not $PrinterXPS) 
+{
+    echo "l'imprimante Microsoft XPS Document Writer n'est pas presente sur le poste"
+}
+else
+{
+    Remove-Printer -Name "Microsoft XPS Document Writer"
+    Write-Output "Supression de l'imprimante Microsoft XPS Document Writer"
+}
+
+# Regarder si l'imprimante OneNote for Windows 10 est presente sur le poste, sinon la desinstalle
+
+if (-not $PrinterONE) 
+{
+    echo "l'imprimante OneNote for Windows 10 n'est pas presente sur le poste"
+}
+else
+{
+    Remove-Printer -Name "OneNote for Windows 10"
+    Write-Output "Supression de l'imprimante OneNote for Windows 10"
+}
+
+# Regarder si l'imprimante Fax est presente sur le poste, sinon la desinstalle
+
+if (-not $PrinterFAX) 
+{
+    echo "l'imprimante Fax n'est pas presente sur le poste"
+}
+else
+{
+    Remove-Printer -Name "Fax"
+    Write-Output "Supression de l'imprimante Fax"
+}
+
+# Regarder si l'imprimante Microsoft Print to PDF est presente sur le poste, sinon la desinstalle
+
+if (-not $PrinterPDF) 
+{
+    echo "l'imprimante Microsoft Print to PDF n'est pas presente sur le poste"
+}
+else
+{
+    Remove-Printer -Name "Microsoft Print to PDF"
+    Write-Output "Supression de l'imprimante Microsoft Print to PDF"
+}
